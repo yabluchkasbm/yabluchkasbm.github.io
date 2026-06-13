dc035f26f2895c05ad694d97bcb31eb3201ee946
smas_buttermetter <pistohova@gmail.com>
2026-06-13 22:28:00 +0300

    Update 1.js

--- present/1.js
+++ present/1.js
@@ -14,8 +14,7 @@ btnHideShow.onclick = function () {
 ScrollReveal().reveal('h1', { reset: true, origin: 'top', delay: 700, distance: '50px' });
 ScrollReveal().reveal('p', { reset: true, delay: 700, origin: 'bottom', distance: '150px' });
 ScrollReveal().reveal('h2', { reset: true, origin: 'top', delay: 700, distance: '50px' });
-ScrollReveal().reveal('h3', { reset: true, origin: 'top', delay: 700, distance: '50px' });
-ScrollReveal().reveal('h5', { reset: true, delay: 700, distance: '50px' });
+ScrollReveal().reveal('h3', { reset: true, origin: 'top', delay: 1000, distance: '600px' });
 
 window.onload = function() {
     setTimeout(function() {
@@ -25,3 +24,13 @@ window.onload = function() {
         }
     }, 1000); // невелика затримка у 0.1 секунди для надійності
 };
+
+
+ScrollReveal().reveal('.card-1', { reset: true, origin: 'top', delay: 700, distance: '100px' });
+ScrollReveal().reveal('.card-2', { reset: true, origin: 'bottom', delay: 800, distance: '150px' });
+ScrollReveal().reveal('.card-3', { reset: true, origin: 'top', delay: 900, distance: '200px' });
+
+ScrollReveal().reveal('.card-4', { reset: true, origin: 'top', delay: 900, distance: '200px' });
+ScrollReveal().reveal('.card-5', { reset: true, origin: 'bottom', delay: 800, distance: '150px' });
+ScrollReveal().reveal('.card-6', { reset: true, origin: 'top', delay: 700, distance: '100px' });
+
